85adf713046b3dc8c14071255d64797f3620076e
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-03-14 09:49:36 -0300

    🐛 Fix install script

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/sh
 
-NVIM_V2=~/.config/nvim-v2
+NVIM_V2=~/.config
+
+SHARE=$NVIM_V2/share
+NVIM=$NVIM_V2/nvim
+
 export NVIM_V2
 
-rm -rf $NVIM_V2
+rm -rf $NVIM $SHARE
 
-mkdir -p $NVIM_V2/share
-mkdir -p $NVIM_V2/nvim
+mkdir -p $NVIM
+mkdir -p $SHARE
 
-stow --restow --target=$NVIM_V2/nvim .
+stow --restow --target=$NVIM .
 
-alias nvi='XDG_DATA_HOME=$NVIM_V2/share XDG_CONFIG_HOME=$NVIM_V2 nvim'
+alias nvi='XDG_DATA_HOME=$SHARE XDG_CONFIG_HOME=$NVIM nvim'
 export nvi
