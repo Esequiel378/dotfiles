7f816ba5a49d87de8961c94d8d2859d5b5ea6113
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-03-14 13:24:24 -0300

    🐛 Fix race confition with mapleader

--- after/plugin/options.lua
+++ after/plugin/options.lua
@@ -1,13 +1,7 @@
-local api = vim.api
 local g = vim.g
 local opt = vim.opt
 local wo = vim.wo
 local bo = vim.bo
-
--- Remap leader and local leader to <Space>
-api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
-g.mapleader = " "
-g.maplocalleader = " "
 
 opt.cul = true -- cursor line
 bo.swapfile = false
--- init.lua
+++ init.lua
@@ -1,1 +1,6 @@
+-- Remap leader and local leader to <Space>
+vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("plugins").setup()
