7ede79354f92170fa2531b00454df33aada15329
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-03-19 15:19:20 -0300

    ⚡Add general plugins for make my life easier

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,155 +4,156 @@ local M = {}
 local packer_bootstrap = false
 
 function M.setup()
-   local present, packer = pcall(require, "packer")
+  local present, packer = pcall(require, "packer")
 
-   if not present then
-      local packer_path = vim.fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
+  if not present then
+    local packer_path = vim.fn.stdpath "data" .. "/site/pack/packer/opt/packer.nvim"
 
-      print "Cloning packer..."
+    print "Cloning packer..."
 
-      -- remove the dir before cloning
-      vim.fn.delete(packer_path, "rf")
-      packer_bootstrap = vim.fn.system {
-         "git",
-         "clone",
-         "https://github.com/wbthomason/packer.nvim",
-         "--depth",
-         "20",
-         packer_path,
-      }
+    -- remove the dir before cloning
+    vim.fn.delete(packer_path, "rf")
+    packer_bootstrap = vim.fn.system {
+      "git",
+      "clone",
+      "https://github.com/wbthomason/packer.nvim",
+      "--depth",
+      "20",
+      packer_path,
+    }
 
-      vim.cmd "packadd packer.nvim"
-      present, packer = pcall(require, "packer")
+    vim.cmd "packadd packer.nvim"
+    present, packer = pcall(require, "packer")
 
-      if present then
-         print "Packer cloned successfully."
-      else
-         error("Couldn't clone packer !\nPacker path: " .. packer_path .. "\n" .. packer)
-      end
-   end
+    if present then
+      print "Packer cloned successfully."
+    else
+      error("Couldn't clone packer !\nPacker path: " .. packer_path .. "\n" .. packer)
+    end
+  end
 
-   vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
+  vim.cmd "autocmd BufWritePost plugins.lua source <afile> | PackerCompile"
 
-   -- packer.nvim configuration
-   local conf = {
-      display = {
-         open_fn = function()
-            return require("packer.util").float { border = "single" }
-         end,
-         prompt_border = "single",
-      },
-      profile = {
-         enable = true,
-         threshold = 0,
-      },
-   }
+  -- packer.nvim configuration
+  local conf = {
+    display = {
+      open_fn = function()
+        return require("packer.util").float { border = "single" }
+      end,
+      prompt_border = "single",
+    },
+    profile = {
+      enable = true,
+      threshold = 0,
+    },
+  }
 
-   -- Performance
-   pcall(require, "impatient")
+  -- Performance
+  pcall(require, "impatient")
 
-   packer.init(conf)
-   packer.startup(M.config)
+  packer.init(conf)
+  packer.startup(M.config)
 
-   return packer
+  return packer
 end
 
 function M.config(use)
-   use { "wbthomason/packer.nvim" }
-   use { "nvim-lua/plenary.nvim", module = "plenary" }
+  use { "wbthomason/packer.nvim" }
+  use { "nvim-lua/plenary.nvim", module = "plenary" }
 
-   -- Performance
-   use { "nathom/filetype.nvim" }
-   use { "lewis6991/impatient.nvim" }
+  -- Performance
+  use { "nathom/filetype.nvim" }
+  use { "lewis6991/impatient.nvim" }
 
-   -- TODO: Organize them
-   use "tpope/vim-surround"
-   use "andymass/vim-matchup"
-   use "shmup/vim-sql-syntax"
-   use "folke/lsp-colors.nvim"
-   use {
-      "norcalli/nvim-colorizer.lua",
-      event = "BufRead",
-      config = function()
-         require("colorizer").setup()
-      end,
-   }
+  -- TODO: Move plugins with configuration to it's own files
+  use { "chr4/nginx.vim" }
+  use { "tpope/vim-surround" }
+  use { "andymass/vim-matchup" }
+  use { "shmup/vim-sql-syntax" }
+  use { "folke/lsp-colors.nvim" }
+  use {
+    "norcalli/nvim-colorizer.lua",
+    event = "BufRead",
+    config = function()
+      require("colorizer").setup()
+    end,
+  }
 
-   use {
-      "simrat39/symbols-outline.nvim",
-      config = function()
-         local map = require("utils").map
-         map("n", "<leader>s", ":SymbolsOutline <CR>")
+  use {
+    "simrat39/symbols-outline.nvim",
+    config = function()
+      local map = require("utils").map
+      map("n", "<leader>s", ":SymbolsOutline <CR>")
 
-         require("symbols-outline").setup {
-            auto_preview = false,
-            position = "left",
-            show_numbers = true,
-            show_relative_numbers = true,
-         }
-      end,
-   }
+      require("symbols-outline").setup {
+        auto_preview = false,
+        position = "left",
+        show_numbers = true,
+        show_relative_numbers = true,
+      }
+    end,
+  }
 
-   -- Markdown
-   use {
-      "iamcco/markdown-preview.nvim",
-      run = function()
-         vim.fn["mkdp#util#install"]()
-      end,
-      ft = "markdown",
-      cmd = { "MarkdownPreview" },
-   }
-   -- END OF TODO!
+  -- Markdown
+  use {
+    "iamcco/markdown-preview.nvim",
+    run = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+    ft = "markdown",
+    cmd = { "MarkdownPreview" },
+  }
+  -- END OF TODO!
 
-   -- Notification
-   require("plugins.config.notification").setup(use)
+  -- Notification
+  require("plugins.config.notification").setup(use)
 
-   -- Treesitter
-   require("plugins.config.treesitter").setup(use)
+  -- Treesitter
+  require("plugins.config.treesitter").setup(use)
 
-   -- Telescope
-   require("plugins.config.telescop").setup(use)
+  -- Telescope
+  require("plugins.config.telescop").setup(use)
 
-   -- Ranger
-   require("plugins.config.ranger").setup(use)
+  -- Ranger
+  require("plugins.config.ranger").setup(use)
 
-   -- LSP
-   require("plugins.config.lsp.init").setup(use)
+  -- LSP
+  require("plugins.config.lsp.init").setup(use)
 
-   -- Completion
-   require("plugins.config.cmp.init").setup(use)
+  -- Completion
+  require("plugins.config.cmp.init").setup(use)
 
-   -- Statusline
-   require("plugins.config.statusline").setup(use)
+  -- Statusline
+  require("plugins.config.statusline").setup(use)
 
-   -- Better icons
-   require("plugins.config.devicons").setup(use)
+  -- Better icons
+  require("plugins.config.devicons").setup(use)
 
-   -- Better Comment
-   require("plugins.config.comments").setup(use)
+  -- Better Comment
+  require("plugins.config.comments").setup(use)
 
-   -- Colorscheme
-   require("plugins.config.colorscheme").setup(use)
+  -- Colorscheme
+  require("plugins.config.colorscheme").setup(use)
 
-   -- Startup screen
-   require("plugins.config.startup_screen").setup(use)
+  -- Startup screen
+  require("plugins.config.startup_screen").setup(use)
 
-   -- Git
-   require("plugins.config.git.init").setup(use)
+  -- Git
+  require("plugins.config.git.init").setup(use)
 
-   -- Golang
-   require("plugins.config.golang").setup(use)
+  -- Golang
+  require("plugins.config.golang").setup(use)
 
-   -- Trouble
-   require("plugins.config.troubl").setup(use)
+  -- Trouble
+  require("plugins.config.troubl").setup(use)
 
-   -- Editorconfig
-   require("plugins.config.editorconfig").setup(use)
+  -- Editorconfig
+  require("plugins.config.editorconfig").setup(use)
 
-   if packer_bootstrap then
-      print "Restart Neovim required after installation!"
-      require("packer").sync()
-   end
+  if packer_bootstrap then
+    print "Restart Neovim required after installation!"
+    require("packer").sync()
+  end
 end
 
 return M
