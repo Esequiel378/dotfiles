5b041b291174c6d24d33ce1a82f0db3ddb4c6ed4
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-05-30 22:08:04 -0300

    ⚡Update statusline config

--- lua/plugins/config/statusline.lua
+++ lua/plugins/config/statusline.lua
@@ -2,95 +2,95 @@ local M = {}
 
 -- Color table for highlights
 local colors = {
-   bg = "#202328",
-   fg = "#bbc2cf",
-   yellow = "#ECBE7B",
-   cyan = "#008080",
-   darkblue = "#081633",
-   green = "#98be65",
-   orange = "#FF8800",
-   violet = "#a9a1e1",
-   magenta = "#c678dd",
-   blue = "#51afef",
-   red = "#ec5f67",
+  bg = "#202328",
+  fg = "#bbc2cf",
+  yellow = "#ECBE7B",
+  cyan = "#008080",
+  darkblue = "#081633",
+  green = "#98be65",
+  orange = "#FF8800",
+  violet = "#a9a1e1",
+  magenta = "#c678dd",
+  blue = "#51afef",
+  red = "#ec5f67",
 }
 
 local function separator()
-   return "%="
+  return "%="
 end
 
 local function lsp_client(msg)
-   msg = msg or ""
-   local buf_clients = vim.lsp.buf_get_clients()
-   if next(buf_clients) == nil then
-      if type(msg) == "boolean" or #msg == 0 then
-         return ""
-      end
-      return msg
-   end
+  msg = msg or ""
+  local buf_clients = vim.lsp.buf_get_clients()
+  if next(buf_clients) == nil then
+    if type(msg) == "boolean" or #msg == 0 then
+      return ""
+    end
+    return msg
+  end
 
-   local buf_ft = vim.bo.filetype
-   local buf_client_names = {}
+  local buf_ft = vim.bo.filetype
+  local buf_client_names = {}
 
-   -- add client
-   for _, client in pairs(buf_clients) do
-      if client.name ~= "null-ls" then
-         table.insert(buf_client_names, client.name)
-      end
-   end
+  -- add client
+  for _, client in pairs(buf_clients) do
+    if client.name ~= "null-ls" then
+      table.insert(buf_client_names, client.name)
+    end
+  end
 
-   -- add linter
-   local linters = require "plugins.config.lsp.null-ls.linters"
-   local supported_linters = linters.list_registered(buf_ft)
-   vim.list_extend(buf_client_names, supported_linters)
+  -- add linter
+  local linters = require "plugins.config.lsp.null-ls.linters"
+  local supported_linters = linters.list_registered(buf_ft)
+  vim.list_extend(buf_client_names, supported_linters)
 
-   return "[" .. table.concat(buf_client_names, ", ") .. "]"
+  return "[" .. table.concat(buf_client_names, ", ") .. "]"
 end
 
 function M.setup(use)
-   use {
-      "nvim-lualine/lualine.nvim",
-      event = "VimEnter",
-      requires = { "nvim-web-devicons" },
-      config = function()
-         require("plugins.config.statusline").config()
-      end,
-   }
+  use {
+    "nvim-lualine/lualine.nvim",
+    event = "VimEnter",
+    requires = { "nvim-web-devicons" },
+    config = function()
+      require("plugins.config.statusline").config()
+    end,
+  }
 end
 
 function M.config()
-   require("lualine").setup {
-      options = {
-         icons_enabled = true,
-         theme = "auto",
-         component_separators = { left = "", right = "" },
-         section_separators = { left = "", right = "" },
-         disabled_filetypes = {},
-         always_divide_middle = true,
+  require("lualine").setup {
+    options = {
+      icons_enabled = true,
+      theme = "auto",
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {},
+      always_divide_middle = true,
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = {
+        { "filename" },
+        { separator },
+        { lsp_client, icon = " ", color = { fg = colors.violet, gui = "bold" } },
       },
-      sections = {
-         lualine_a = { "mode" },
-         lualine_b = { "branch", "diff", "diagnostics" },
-         lualine_c = {
-            { "filename" },
-            { separator },
-            { lsp_client, icon = " ", color = { fg = colors.violet, gui = "bold" } },
-         },
-         lualine_x = { "encoding", "fileformat", "filetype" },
-         lualine_y = { "progress" },
-         lualine_z = { "location" },
-      },
-      inactive_sections = {
-         lualine_a = {},
-         lualine_b = {},
-         lualine_c = { "filename" },
-         lualine_x = { "location" },
-         lualine_y = {},
-         lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
-   }
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    extensions = {},
+  }
 end
 
 return M
