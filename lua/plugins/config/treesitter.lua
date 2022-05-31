8d17e3d6f2b9ddd1ab075c4149cddee4dda0e898
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-05-30 22:09:36 -0300

    ⚡Format treesitter config file

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,39 +1,39 @@
 local M = {}
 
 function M.setup(use)
-   use {
-      "nvim-treesitter/nvim-treesitter",
-      run = ":TSUpdate",
-      config = function()
-         require("plugins.config.treesitter").config()
-      end,
-   }
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+    config = function()
+      require("plugins.config.treesitter").config()
+    end,
+  }
 end
 
 function M.config()
-   require("nvim-treesitter.configs").setup {
-      -- One of "all", "maintained" (parsers with maintainers), or a list of languages
-      ensure_installed = {
-         "lua",
-      },
+  require("nvim-treesitter.configs").setup {
+    -- One of "all", "maintained" (parsers with maintainers), or a list of languages
+    ensure_installed = {
+      "lua",
+    },
 
-      -- Install languages synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-      autopairs = {
-         enable = true,
-      },
-      highlight = {
-         enable = true,
-         use_languagetree = true,
-         additional_vim_regex_highlighting = true,
-      },
-      context_commentstring = {
-         enable = true,
-      },
-      endwise = {
-         enable = true,
-      },
-   }
+    -- Install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    autopairs = {
+      enable = true,
+    },
+    highlight = {
+      enable = true,
+      use_languagetree = true,
+      additional_vim_regex_highlighting = true,
+    },
+    context_commentstring = {
+      enable = true,
+    },
+    endwise = {
+      enable = true,
+    },
+  }
 end
 
 return M
