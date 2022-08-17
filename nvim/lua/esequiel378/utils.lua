493eed4189b53859f0e4ad1ba2007b8a3da07291
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-08-17 20:00:20 -0300

    ⚡Add loggin funtions to utils

--- nvim/lua/esequiel378/utils.lua
+++ nvim/lua/esequiel378/utils.lua
@@ -49,4 +49,22 @@ M.map = function(mode, keys, cmd, opt)
    map_wrapper(mode, keys, cmd, options)
 end
 
+function M.log(msg, hl, name)
+   name = name or "Neovim"
+   hl = hl or "Todo"
+   vim.api.nvim_echo({ { name .. ": ", hl }, { msg } }, true, {})
+end
+
+function M.warn(msg, name)
+   vim.notify(msg, vim.log.levels.WARN, { title = name })
+end
+
+function M.error(msg, name)
+   vim.notify(msg, vim.log.levels.ERROR, { title = name })
+end
+
+function M.info(msg, name)
+   vim.notify(msg, vim.log.levels.INFO, { title = name })
+end
+
 return M
