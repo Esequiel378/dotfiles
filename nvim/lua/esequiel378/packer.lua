e0cd71ba3b44f75eda200b9483f69df9e0d40b6f
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2022-08-17 12:21:44 -0300

    :zap: Add lsp support

--- nvim/lua/esequiel378/packer.lua
+++ nvim/lua/esequiel378/packer.lua
@@ -13,7 +13,7 @@ function M.setup()
 
 		-- remove the dir before cloning
 		vim.fn.delete(packer_path, "rf")
-		packer_bootstrap = vim.fn.system {
+		vim.fn.system {
 			"git",
 			"clone",
 			"https://github.com/wbthomason/packer.nvim",
@@ -21,6 +21,8 @@ function M.setup()
 			"20",
 			packer_path,
 		}
+
+		packer_bootstrap = true
 
 		vim.cmd [[packadd packer.nvim]]
 		present, packer = pcall(require, "packer")
@@ -32,9 +34,9 @@ function M.setup()
 		end
 	end
 
-	-- TODO: Check if I can re-comple packer after saving any lua file
-	-- vim.cmd "autocmd BufWritePost plugins.init.lua source <afile> | PackerCompile"
-	vim.cmd "autocmd BufWritePost *.lua source <afile> | PackerCompile"
+	-- TODO: Check if it makes sence to recompile the file on save (since M.config won't be called)
+	-- vim.cmd "autocmd BufWritePost *.lua source <afile> | PackerCompile"
+	-- vim.cmd "autocmd BufWritePost esequiel378.packer source <afile> | PackerCompile"
 
 	-- packer.nvim configuration
 	local conf = {
@@ -70,26 +72,52 @@ function M.config(use)
 	use { "nvim-lua/plenary.nvim", module = "plenary" }
 
 	-- extend vim's builtins
-  use { "tpope/vim-surround" }
-  use { "andymass/vim-matchup" }
+	use { "tpope/vim-surround" }
+	use { "andymass/vim-matchup" }
 
 	-- colors
-  use { "folke/lsp-colors.nvim" }
-  use {
+	use { "folke/lsp-colors.nvim" }
+	use {
 		"shmup/vim-sql-syntax",
 		ft = "sql",
 	}
-  use {
+	use {
 		"mechatroner/rainbow_csv",
 		ft = "csv",
 	}
-  use {
-    "norcalli/nvim-colorizer.lua",
-    event = "BufRead",
-    config = function()
-      require("colorizer").setup()
-    end,
-  }
+	use {
+		"norcalli/nvim-colorizer.lua",
+		event = "BufRead",
+		config = function()
+			require("colorizer").setup()
+		end,
+	}
+
+	use {
+		"bluz71/vim-moonfly-colors",
+		config = function()
+			vim.cmd [[colorscheme moonfly]]
+			-- vim.cmd "hi Normal guibg=0000"
+		end,
+	}
+
+	use {
+		"tpope/vim-commentary",
+		keys = { "gc", "gcc", "gbc" },
+	}
+
+	use {
+		"rcarriga/nvim-notify",
+		event = "VimEnter",
+		config = function()
+			vim.notify = require "notify"
+
+			require("notify").setup {
+				background_colour = "#000000",
+			}
+		end,
+	}
+
 
 	require("esequiel378.plugins.treesitter").setup(use)
 	require("esequiel378.plugins.alpha-nvim").setup(use)
@@ -97,6 +125,7 @@ function M.config(use)
 	require("esequiel378.plugins.trouble").setup(use)
 	require("esequiel378.plugins.telescope").setup(use)
 	require("esequiel378.plugins.git.init").setup(use)
+	require("esequiel378.plugins.lsp.init").setup(use)
 
 	if packer_bootstrap then
 		print "Restart Neovim required after installation!"
