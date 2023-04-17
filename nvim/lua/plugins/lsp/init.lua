5be6a67eaa75e3486c091eeb43b90011654b0497
Esequiel Albornoz <esequielalbonoz7@gmail.com>
2023-04-17 09:57:25 -0300

    :zap: Add null-ls builtins

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,11 +1,31 @@
+local with_diagnostics_code = function(builtin)
+  return builtin.with {
+    diagnostics_format = "#{m} [#{c}]",
+  }
+end
+
+local with_root_file = function(builtin, file)
+  return builtin.with {
+    condition = function(utils)
+      return utils.root_has_file(file)
+    end,
+  }
+end
+
+local with_extra_args = function(builtin, args)
+  return builtin.with {
+    extra_args = args,
+  }
+end
+
 return {
   {
     "neovim/nvim-lspconfig",
     event = "BufReadPre",
     dependencies = {
-      { "folke/neoconf.nvim", cmd = "Neoconf", config = true },
-      { "folke/neodev.nvim", config = true },
-      { "j-hui/fidget.nvim", config = true },
+      { "folke/neoconf.nvim",      cmd = "Neoconf", config = true },
+      { "folke/neodev.nvim",       config = true },
+      { "j-hui/fidget.nvim",       config = true },
       { "smjonas/inc-rename.nvim", config = true },
       "simrat39/rust-tools.nvim",
       "rust-lang/rust.vim",
@@ -50,6 +70,7 @@ return {
     ensure_installed = {
       "stylua",
       "ruff",
+      "delve",
     },
     config = function(plugin)
       require("mason").setup()
@@ -70,8 +91,37 @@ return {
       local nls = require "null-ls"
       nls.setup {
         sources = {
+          -- formatting
           nls.builtins.formatting.stylua,
-          nls.builtins.diagnostics.ruff.with { extra_args = { "--max-line-length=90" } },
+          nls.builtins.formatting.isort,
+          nls.builtins.formatting.nginx_beautifier,
+          nls.builtins.formatting.prettier,
+          nls.builtins.formatting.shfmt,
+          with_extra_args(nls.builtins.diagnostics.ruff, { "--max-line-length=90" }),
+          with_extra_args(nls.builtins.formatting.fixjson, { "-i", "2" }),
+          with_extra_args(nls.builtins.formatting.black, { "--fast" }),
+          with_root_file(nls.builtins.formatting.stylua, "stylua.toml"),
+          with_extra_args(nls.builtins.formatting.sql_formatter, {
+            "--config",
+            vim.fn.expand "~/.config/nvim/lua/esequiel378/plugins/lsp/config/sql-formatter.json",
+          }),
+
+          -- diagnostics
+          nls.builtins.diagnostics.eslint_d,
+          nls.builtins.diagnostics.misspell,
+          nls.builtins.diagnostics.pyproject_flake8,
+          nls.builtins.diagnostics.tsc,
+          nls.builtins.diagnostics.write_good,
+
+          with_diagnostics_code(nls.builtins.diagnostics.shellcheck),
+          with_root_file(nls.builtins.diagnostics.selene, "selene.toml"),
+
+          -- code actions
+          nls.builtins.code_actions.eslint_d,
+          nls.builtins.code_actions.proselint,
+
+          -- hover
+          nls.builtins.hover.dictionary,
         },
       }
     end,
