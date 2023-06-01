19e9bfe30aa3edca372941207c66aa7a1782bc29
Esequiel Albornoz <esequielalbonoz7@gmail.com>
2023-06-01 15:57:19 -0300

    🐛Fix telescope opening files in insert mode

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -68,3 +68,12 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
     end
   end,
 })
+
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2027#issuecomment-1561836585
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
