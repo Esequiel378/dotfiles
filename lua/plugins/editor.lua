171aac6f466eff2b547324c7513afcfd7ff837af
Esequiel <esequielalbornoz7@gmail.com>
2024-12-18 17:01:57 -0300

    :zap: Add python selector

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -55,4 +55,18 @@ return {
 			{ "<leader>gs", "<cmd>:G<CR><C-w>T", desc = "Git Status" },
 		},
 	},
+	{
+		"linux-cultist/venv-selector.nvim",
+		dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+		config = function()
+			require("venv-selector").setup({})
+		end,
+		event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+		keys = {
+			-- Keymap to open VenvSelector to pick a venv.
+			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+			-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+			{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+		},
+	},
 }
