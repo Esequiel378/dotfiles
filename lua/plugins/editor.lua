440289b19a29c5642f3557704e2acc9632165de1
Esequiel <esequielalbornoz7@gmail.com>
2024-12-18 17:09:12 -0300

    :zap: Add oil for better fs actions

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -5,6 +5,19 @@ return {
 	{ "tpope/vim-sleuth", event = "VeryLazy" },
 	{ "tpope/vim-repeat", event = "VeryLazy" },
 	{
+		"stevearc/oil.nvim",
+		---@module 'oil'
+		---@type oil.SetupOpts
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		-- Optional dependencies
+		dependencies = { { "echasnovski/mini.icons", opts = {} } },
+		-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	},
+	{
 		"editorconfig/editorconfig-vim",
 		lazy = false,
 		config = function()
